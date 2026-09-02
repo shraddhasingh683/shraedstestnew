321efc93dee481ec87c2dc36c2eefa72c490fcbc
shraddhasingh683 <shraddhasingh683@gmail.com>
2026-09-02 21:10:36 +0530

    Update constant.js

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -66,7 +66,7 @@ export const defaultErrorMessages = {
 // eslint-disable-next-line no-useless-escape
 export const emailPattern = '([A-Za-z0-9][._]?)+[A-Za-z0-9]@[A-Za-z0-9]+(\.?[A-Za-z0-9]){2}\.([A-Za-z0-9]{2,4})?';
 
-let submitBaseUrl = 'https://publish-p148860-e338290-cmstg.adobeaemcloud.com';
+let submitBaseUrl = '';
 
 export const SUBMISSION_SERVICE = 'https://forms.adobe.com/adobe/forms/af/submit/';
 
